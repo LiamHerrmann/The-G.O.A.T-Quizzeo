b3c3a2d6325a004646e2fc2ffb1b2d4bc2801f3e
Mel-Meli <m.lefils@ipssi-ecole.net>
2024-04-30 16:48:33 +0200

    api init

---
+++ src/controllers/quiz.controllers.js
@@ -0,0 +1,14 @@
+const db = require('../database/database');
+
+// Fonction récupérer un quiz
+
+// Fonction créer un quiz
+
+// Fonction supression d'article
+
+//ajout module
+module.exports = {
+	get **,
+	post **,
+	delete **,
+};
