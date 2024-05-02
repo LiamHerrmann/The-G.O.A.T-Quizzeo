f8f048abdd24a0cbcc5774027a89378685c9a22e
Mel-Meli <m.lefils@ipssi-ecole.net>
2024-05-02 12:07:34 +0200

    avancement 75%

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const db = require(../database/database);
+const db = require('../database/database');
 
 app.listen(8080,() => {
     console.log("Serveur is up")
